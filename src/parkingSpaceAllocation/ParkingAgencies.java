39704b81d97255bb48f38c4f1824495d54b60e64
UristMcUrist <scylor@gmail.com>
2016-12-06 20:28:05 +0100

    Deleted unecessary

--- src/parkingSpaceAllocation/ParkingAgencies.java
+++ src/parkingSpaceAllocation/ParkingAgencies.java
@@ -5,5 +5,5 @@ public interface ParkingAgencies  {
 	public boolean checkin(int id,int checkin);
 	public double checkout(int id,int checkout);
 	public double getPrice(int day, int starttime, int staytime);
-	
+	public double getDisdtance(Position driverposition);
 }
