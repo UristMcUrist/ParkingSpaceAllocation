ed8a15398dc19868d77b6263f5ac13e177701545
Thomas <th.vrancken@hotmail.com>
2016-12-06 20:09:27 +0100

    ok

--- src/parkingSpaceAllocation/Driver.java
+++ src/parkingSpaceAllocation/Driver.java
@@ -1,11 +1,10 @@
-
+package parkingSpaceAllocation;
 
 public interface Driver {
 	public void move(int direction);
-	public void findOptimalParking();
 	public GuidedDriver(Context<Object> context, Grid<Object> grid, int ID, int desX, int desY, int arrivalT, int maxPrice, int duration, int maxWalking, int DAY, boolean cheapOrNot, boolean lazyOrNot);
 	public void step();
-	public double findOptimalParking(Knowledge k);
+	public double findOptimalParking();
 	public int getDirectionToParking(ParkingAgent par);
 }
 
